209a025d0fbc4e6e0e733446ecad96aa37cc88aa
katerine <katerine.munoz02@gmail.com>
2019-09-03 21:06:51 -0500

    Interface

--- src/main/java/com/accenture/rentacar/app/entity/Vehiculo.java
+++ src/main/java/com/accenture/rentacar/app/entity/Vehiculo.java
@@ -1,56 +1,97 @@
 package com.accenture.rentacar.app.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+@Table(name="vehiculos")
 
 
 public class Vehiculo {
       
 	@Id
+	@GeneratedValue
+	(strategy = GenerationType.IDENTITY) //Estrategia de generación de ese ID
 	private Long id;
-	private short modelo;
+	
+	@Column(name="modelo_auto") 
+	private short modeloAuto;
 	private String marca;
 	private String placa;
 	private String linea;
 	private String color;
 	
+	@Column(name="fechaCreacionRegistro")
+	@Temporal(TemporalType.DATE)
+	private Date fechaCreacionRegistro;
+	
+	@PrePersist
+	private void preGuardado() { //Se llama como quiera, se busca
+		fechaCreacionRegistro = new Date(); 
+	}
+	
+	public Date getFechaCreacionRegistro() {
+		return fechaCreacionRegistro;
+	}
+	
+	public void setFechaCreacionRegistro(Date fechaCreacionRegistro) {
+		this.fechaCreacionRegistro = fechaCreacionRegistro;
+	}
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public short getModelo() {
-		return modelo;
+	
+	public short getModeloAuto() {
+		return modeloAuto;
 	}
-	public void setModelo(short modelo) {
-		this.modelo = modelo;
+	
+	public void setModeloAuto(short modeloAuto) {
+		this.modeloAuto = modeloAuto;
 	}
+	
 	public String getMarca() {
 		return marca;
 	}
+	
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+	
 	public String getPlaca() {
 		return placa;
 	}
+
 	public void setPlaca(String placa) {
 		this.placa = placa;
 	}
+	
 	public String getLinea() {
 		return linea;
 	}
+	
 	public void setLinea(String linea) {
 		this.linea = linea;
 	}
+	
 	public String getColor() {
 		return color;
 	}
+	
 	public void setColor(String color) {
 		this.color = color;
 	}
-
 }
---
+++ src/main/java/com/accenture/rentacar/app/service/IVehiculoService.java
@@ -0,0 +1,17 @@
+package com.accenture.rentacar.app.service;
+
+import java.util.List;
+
+import com.accenture.rentacar.app.entity.Vehiculo;
+
+public interface IVehiculoService {
+	public Vehiculo save( Vehiculo vehiculo );
+	
+	public void delete( Long id );
+	
+	public Vehiculo buscarVehiculoPorId( Long id );
+	
+	public List<Vehiculo> listarTodos();
+}
+
+
