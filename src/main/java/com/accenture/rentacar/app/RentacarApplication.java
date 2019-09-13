da670a2f8800d28e3263d547714f7e195ee9d492
katerine <katerine.munoz02@gmail.com>
2019-09-12 20:42:06 -0500

    sep12

--- src/main/java/com/accenture/rentacar/app/RentacarApplication.java
+++ src/main/java/com/accenture/rentacar/app/RentacarApplication.java
@@ -1,13 +1,20 @@
 package com.accenture.rentacar.app;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.accenture.utilidades.fechas.Fecha;
 
 @SpringBootApplication
 public class RentacarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RentacarApplication.class, args);
+		//SpringApplication.run(RentacarApplication.class, args);
+																								    //new Date()
+		//System.out.println("Resultado: " + Fecha.calcularDias(new Date("12/05/2018"), new Date())); //INSTRUCCIÓN CON AMARILLO; Método obsoleto(vulnerabilidad de seguridad o mejor forma de hacer el método)
+				
 	}
 
 }
